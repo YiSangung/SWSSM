f086c5563c417ed352515095b389c348e5aec5e3
YiSangung <delphi119@gmail.com>
2016-01-19 17:13:06 +0900

    Initialize #2

--- iot/src/main/java/org/secmem/swssm/iot/Bootstrap.java
+++ iot/src/main/java/org/secmem/swssm/iot/Bootstrap.java
@@ -1,7 +1,10 @@
 package org.secmem.swssm.iot;
 
 public class Bootstrap {
+
     public static void main(String[] args) {
         System.out.println("[BEGIN]");
+
+        System.out.println("[END]");
     }
 }
